8340a45fa777596bde650f56b77d449dd2960e1d
darwish.michael@gmail.com <darwish.michael@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-10-14 00:44:00 +0000

    Initial commit.

---
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/Game1.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Tron
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		Grid grid;
+		Cycle cycle;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			//graphics.IsFullScreen = true;
+			IsMouseVisible = true;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			grid = new Grid(new Vector2(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height));
+			cycle = new Cycle() { Grid = grid };
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			Art.LoadContent(Content);
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			CKeyboard.Update(gameTime);
+
+			if (CKeyboard.WasPressed(Keys.F2))
+				Initialize();
+			else if (CKeyboard.WasPressed(Keys.P))
+				cycle.Paused = !cycle.Paused;
+
+			cycle.HandleInput(Keyboard.GetState());
+			cycle.Update();
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+			grid.Draw(spriteBatch);
+			cycle.Draw(spriteBatch);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/Line.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Tron
+{
+	class Line
+	{
+		Vector2 Start { get; set; }
+		Vector2 End { get; set; }
+
+		public Line(Vector2 start, Vector2 end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public static Vector2? FindIntersection(Line a, Line b)
+		{
+			Vector2 point1 = a.Start, point2 = a.End, point3 = b.Start, point4 = b.End;
+			float ua = (point4.X - point3.X) * (point1.Y - point3.Y) - (point4.Y - point3.Y) * (point1.X - point3.X);
+			float ub = (point2.X - point1.X) * (point1.Y - point3.Y) - (point2.Y - point1.Y) * (point1.X - point3.X);
+			float denominator = (point4.Y - point3.Y) * (point2.X - point1.X) - (point4.X - point3.X) * (point2.Y - point1.Y);
+
+			if (denominator == 0)
+				return null;
+
+			ua /= denominator;
+			ub /= denominator;
+
+			if (ua >= 0 && ua <= 1 && ub >= 0 && ub <= 1)
+			{
+				Vector2 intersect = new Vector2();
+				intersect.X = point1.X + ua * (point2.X - point1.X);
+				intersect.Y = point1.Y + ua * (point2.Y - point1.Y);
+				return intersect;
+			}
+
+			return null;
+		}
+	}
+}
