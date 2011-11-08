f619096d2e40b939ce0d3f54f772103c87ded4ca
alennartz@gmail.com <alennartz@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-08 02:02:52 +0000

    Improved AI and fixed a glitch in line creating that was causing faulty colision detection

--- trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Core/Players/AIPlayer.cs
+++ trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Core/Players/AIPlayer.cs
@@ -17,6 +17,8 @@ namespace Cyclotron2D.Core.Players
             
         }
 
+        #region Public Methods
+
         public override void Initialize(Cycle cycle, int id)
         {
             base.Initialize(cycle, id);
@@ -25,15 +27,90 @@ namespace Cyclotron2D.Core.Players
 
         }
 
+
+      
+
         /// <summary>
-        /// The AI is currently very stupid, it randomly turns every 3 seconds
+        /// The AI is getting smarter o.O
         /// </summary>
         /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            bool turned = AvoidWalls();
 
-            var dirs = new[] {-1, 1, -2, 2};
+            if(!turned)
+            {
+                NonRetartedRandomTurn(gameTime);
+            }
+            else
+            {
+                m_lastTurn = gameTime.TotalGameTime;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool AvoidWalls()
+        {
+            int safeLimit = 2;
+
+            int width = Game.GraphicsDevice.Viewport.Bounds.Width;
+            int height = Game.GraphicsDevice.Viewport.Bounds.Height;
+            bool turned = false;
+            switch (Cycle.Direction)
+            {
+                case Direction.Up:
+                    if (Cycle.Position.Y < safeLimit)
+                    {
+
+                        Direction turn = Cycle.Position.X > width / 2 ? Direction.Left : Direction.Right;
+
+                        InvokeDirectionChange(new DirectionChangeEventArgs(turn, Cycle.GetNextGridCrossing()));
+                        turned = true;
+                    }
+                    break;
+                case Direction.Down:
+                    if (Cycle.Position.Y > height-safeLimit)
+                    {
+
+                        Direction turn = Cycle.Position.X > width / 2 ? Direction.Left : Direction.Right;
+
+                        InvokeDirectionChange(new DirectionChangeEventArgs(turn, Cycle.GetNextGridCrossing()));
+                        turned = true;
+                    }
+                    break;
+                case Direction.Left:
+                    if (Cycle.Position.X < safeLimit)
+                    {
+
+                        Direction turn = Cycle.Position.Y > height / 2 ? Direction.Up : Direction.Down;
+
+                        InvokeDirectionChange(new DirectionChangeEventArgs(turn, Cycle.GetNextGridCrossing()));
+                        turned = true;
+                    }
+                    break;
+                case Direction.Right:
+                    if (Cycle.Position.Y > width - safeLimit)
+                    {
+
+                        Direction turn = Cycle.Position.Y > height / 2 ? Direction.Up : Direction.Down;
+
+                        InvokeDirectionChange(new DirectionChangeEventArgs(turn, Cycle.GetNextGridCrossing()));
+                        turned = true;
+                    }
+                    break;
+            }
+
+            return turned;
+
+        }
+
+        private void NonRetartedRandomTurn(GameTime gameTime)
+        {
+            var dirs = new[] { Direction.Left, Direction.Up, Direction.Down, Direction.Right };
 
             if (m_lastTurn == new TimeSpan(0))
                 m_lastTurn = gameTime.TotalGameTime;
@@ -42,22 +119,45 @@ namespace Cyclotron2D.Core.Players
             {
                 if (Cycle.Direction == Direction.Up || Cycle.Direction == Direction.Down)
                 {
-                    dirs = new[] {-1, 1};
+                    if (Cycle.Position.X < 10)
+                    {
+                        dirs = new []{Direction.Right};
+                    }else if (Cycle.Position.X > Game.GraphicsDevice.Viewport.Bounds.Width-10)
+                    {
+                        dirs = new [] { Direction.Left };
+                    }
+                    else
+                    {
+                        dirs = new [] { Direction.Right, Direction.Left };
+                    }
                 }
                 else
                 {
-                    dirs = new[] {-2, 2};
+                    if (Cycle.Position.Y < 10)
+                    {
+                        dirs = new[] { Direction.Down };
+                    }
+                    else if (Cycle.Position.Y > Game.GraphicsDevice.Viewport.Bounds.Height - 10)
+                    {
+                        dirs = new[] { Direction.Up };
+                    }
+                    else
+                    {
+                        dirs = new[] { Direction.Down, Direction.Up };
+                    }
                 }
 
-                int i = m_rand.Next(0, dirs.Length*2);
+                int i = m_rand.Next(0, dirs.Length * 3);
 
                 if (i < dirs.Length)
                 {
-                     InvokeDirectionChange(new DirectionChangeEventArgs((Direction) dirs[m_rand.Next(0, dirs.Length)], Cycle.GetNextGridCrossing()));
+                    InvokeDirectionChange(new DirectionChangeEventArgs(dirs[m_rand.Next(0, dirs.Length)], Cycle.GetNextGridCrossing()));
                 }
-               
+
                 m_lastTurn = gameTime.TotalGameTime;
             }
         }
+
+        #endregion
     }
 }
