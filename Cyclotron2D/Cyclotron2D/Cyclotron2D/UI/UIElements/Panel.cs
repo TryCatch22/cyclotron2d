b7429ae437d490533f0ccaa5ff52eece80064ff2
alennartz@gmail.com <alennartz@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-10 04:27:16 +0000

    wtf Ankh svn glitch removed these files from source control on its own ...

---
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/UI/UIElements/Panel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Cyclotron2D.Screens.Base;
+using Microsoft.Xna.Framework;
+
+namespace Cyclotron2D.UI.UIElements
+{
+    public abstract class Panel : UIElement
+    {
+        protected List<UIElement> Items { get; private set; }
+
+        public int ItemSpacing { get; set; }
+
+        protected Panel(Game game, Screen screen) : base(game, screen)
+        {
+            Items = new List<UIElement>();
+            ItemSpacing = 2; //default value
+        }
+
+        public virtual void AddItems(params UIElement[] item)
+        {
+            Items.AddRange(item);
+        }
+
+        public virtual void RemoveItem(MenuItem item)
+        {
+            Items.Remove(item);
+            item.Dispose();
+        }
+
+        #region IDisposable
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && Items != null)
+            {
+                foreach (UIElement element in Items)
+                {
+                    element.Dispose();
+                }
+                Items = null;
+            }
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
---
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/UI/UIElements/StretchPanel.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Cyclotron2D.Screens.Base;
+using Microsoft.Xna.Framework;
+
+namespace Cyclotron2D.UI.UIElements
+{
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
+    /// <summary>
+    /// Contains
+    /// </summary>
+    public class StretchPanel : Panel
+    {
+
+        public Orientation Orientation { get; set; }
+
+
+        public StretchPanel(Game game, Screen screen) : base(game, screen)
+        {
+            Orientation = Orientation.Vertical;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            int height = (Rect.Height - (Items.Count + 1)*ItemSpacing)/Items.Count;
+            int width = (Rect.Width - (Items.Count + 1) * ItemSpacing) / Items.Count;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Items[i].Rect = Orientation == Orientation.Vertical ? 
+                    new Rectangle(Rect.X, Rect.Y + i*(height + ItemSpacing), Rect.Width, height) : 
+                    new Rectangle(Rect.X + i * (width + ItemSpacing),Rect.Y, width, Rect.Height);
+            }
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            foreach (var uiElement in Items.Where(itm => itm.Visible))
+            {
+                uiElement.Draw(gameTime);
+            }
+        }
+    }
+}
