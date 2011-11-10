ecbdb6c7062e854cbfaf71b01e426eb639cfb4df
alennartz@gmail.com <alennartz@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-10 04:27:16 +0000

    wtf Ankh svn glitch removed these files from source control on its own ...

---
+++ trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/UI/UIElements/LabelTextBox.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Cyclotron2D.Screens.Base;
+using Microsoft.Xna.Framework;
+
+namespace Cyclotron2D.UI.UIElements
+{
+    public sealed class LabelTextBox : UIElement
+    {
+        public String LabelText { get { return LabelElement.Text; } set { LabelElement.Text = value; } }
+        //for convenience
+        public String BoxText { get { return BoxElement.Text; } set { BoxElement.Text = value; } }
+
+       //made these public for easy acces to their own properties like background or text color.
+        public TextElement LabelElement { get; private set; }
+        public TextBox BoxElement { get; private set; }
+
+        public LabelTextBox(Game game, Screen screen) : base(game, screen)
+        {
+            LabelElement = new TextElement(game, screen);
+            BoxElement = new TextBox(game, screen);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Vector2 lblsize = Art.Font.MeasureString(LabelElement.Text) + new Vector2(2, 2);
+            LabelElement.Rect = new Rectangle(Rect.X, Rect.Y, (int) lblsize.X, Rect.Height);
+            BoxElement.Rect = new Rectangle(Rect.X + LabelElement.Rect.Width, Rect.Y, Rect.Width - LabelElement.Rect.Width, Rect.Height);
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            if (BoxElement.Visible)
+            {
+                BoxElement.Draw(gameTime);
+            }
+            if (LabelElement.Visible)
+            {
+                LabelElement.Draw(gameTime);
+            }       
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && BoxElement != null && LabelElement != null)
+            {
+                LabelElement.Dispose();
+                BoxElement.Dispose();
+
+                BoxElement = null;
+                LabelElement = null;
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
---
+++ trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/UI/UIElements/TextBox.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Cyclotron2D.Screens.Base;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cyclotron2D.UI.UIElements
+{
+    public class TextBox :TextElement
+    {
+
+        private bool m_readingText;
+
+        public TextBox(Game game, Screen screen) : base(game, screen)
+        {
+            Background = Color.White;
+        }
+
+        private TimeSpan m_lastDelete;
+
+        private TimeSpan m_repeatTime = new TimeSpan(0, 0, 0, 0, 70);
+
+        private void ReadText(GameTime gameTime)
+        {
+            var input = Game.InputState;
+            char? c = null;
+            if (input.IsNewCharPress(out c))
+            {
+                Text += c.Value;
+            }
+
+            if (Text != "")
+            {
+                if (input.IsNewKeyPress(Keys.Back))
+                {
+                     Text = Text.Remove(Text.Length - 1);
+                    m_lastDelete = gameTime.TotalGameTime;
+                }
+                else if (input.IsKeyDown(Keys.Back) && gameTime.TotalGameTime - m_lastDelete > m_repeatTime)
+                {
+                    Text = Text.Remove(Text.Length - 1);
+                    m_lastDelete = gameTime.TotalGameTime;
+                }
+            }
+
+          
+
+        }
+
+        protected override void HandleInupt(GameTime gameTime)
+        {
+            base.HandleInupt(gameTime);
+
+            var input = Game.InputState;
+            if (input.IsNewLeftClick)
+            {
+                m_readingText = IsMouseOver;
+            }
+
+
+            if (m_readingText)
+            {
+                ReadText(gameTime);
+            }
+
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+
+            if (!m_readingText)
+            {
+                Game.SpriteBatch.Draw(Art.Pixel, Rect, new Color(50, 50, 50, 170));
+            }
+
+            if (ShouldDrawText(typeof(TextBox)))
+            {
+                DrawText();
+            }
+        }
+
+    }
+}
