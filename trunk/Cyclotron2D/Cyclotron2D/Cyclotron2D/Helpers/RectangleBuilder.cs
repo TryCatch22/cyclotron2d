c2c8ea624be74aa26aa793f8bdf77089843a1a5d
alennartz@gmail.com <alennartz@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-19 20:22:30 +0000

    Settings now UI element so we can reuse in multiplayer

--- trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Helpers/RectangleBuilder.cs
+++ trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Helpers/RectangleBuilder.cs
@@ -20,6 +20,20 @@ namespace Cyclotron2D.Helpers
             return new Rectangle(source.X + (source.Width - w)/2, source.Y + (source.Height - h)/2, w, h);
         }
 
+        /// <summary>
+        /// builds a rectangle inside another, centered at the top and with size relative to the source rectangle
+        /// </summary>
+        /// <param name="source">source rectangle to build from</param>
+        /// <param name="dimR">ratios for width and height from source to new</param>
+        /// <returns>A Rectangle at the top of the source</returns>
+        public static Rectangle Top(Rectangle source, Vector2 dimR)
+        {
+            int w = (int)(source.Width * dimR.X), h = (int)(source.Height * dimR.Y);
+            return new Rectangle(source.X + (source.Width - w) / 2, source.Y, w, h);
+        }
+
+
+
         public static Rectangle BottomRight(Rectangle source, Vector2 dimR, Point padding)
         {
             int w = (int)(source.Width * dimR.X), h = (int)(source.Height * dimR.Y);
--- trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Screens/Main/SettingsScreen.cs
+++ trunk/Cyclotron2D/Cyclotron2D/Cyclotron2D/Screens/Main/SettingsScreen.cs
@@ -1,138 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using Cyclotron2D.Helpers;
+﻿using Cyclotron2D.Helpers;
 using Cyclotron2D.Mod;
 using Cyclotron2D.Screens.Base;
 using Cyclotron2D.UI;
-using Cyclotron2D.UI.UIElements;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Cyclotron2D.Screens.Main
 {
     /// <summary>
-    /// this class needs refactoring with a new class Setting to represent a single setting
+    /// Main screen for settings editing
     /// </summary>
     public class SettingsScreen : MainScreen
     {
 
+        #region Fields
 
-        private LabelTextBox m_gridSize;
-        private LabelTextBox m_cycleSpeed;
-        private LabelTextBox m_maxTailLength;
-        private LabelCheckBox m_suicide;
-        private LabelCheckBox m_drawGrid;
-        private LabelCheckBox m_plasmaGrid;
-        private LabelTextBox m_playerName;
+        private SettingsChanger m_settingsView;
 
+        #endregion
 
-        private StretchPanel m_OptionsPanel;
-
-        private CancelOk m_cancelOk;
-
+        #region Constructor
 
         public SettingsScreen(Game game) : base(game, GameState.ChangingSettings)
         {
+            m_settingsView = new SettingsChanger(game, this);
 
-            m_drawGrid = new LabelCheckBox(game, this);
-            m_plasmaGrid = new LabelCheckBox(game, this);
-            m_OptionsPanel = new StretchPanel(game, this);
-            m_gridSize = new LabelTextBox(game, this);
-            m_cycleSpeed = new LabelTextBox(game, this);
-            m_maxTailLength = new LabelTextBox(game, this);
-            m_suicide = new LabelCheckBox(game, this);
-            m_playerName = new LabelTextBox(game, this);
-
-            m_OptionsPanel.AddItems(m_gridSize, m_cycleSpeed, m_maxTailLength, m_suicide, m_drawGrid, m_plasmaGrid, m_playerName);
-
-          
         }
+
+        #endregion
+
+        #region Public Methods
 
         public override void Initialize()
         {
             base.Initialize();
-            
-            m_gridSize.LabelText = Settings.Current.GridSize.ToString();
-            m_cycleSpeed.LabelText = Settings.Current.CycleSpeed.ToString();
-            m_maxTailLength.LabelText = Settings.Current.MaxTailLength.ToString();
-            m_suicide.LabelText = Settings.Current.AllowSuicide.ToString();
-            m_drawGrid.LabelText = Settings.Current.DrawGrid.ToString();
-            m_playerName.LabelText = Settings.Current.PlayerName.ToString();
-            m_plasmaGrid.LabelText = Settings.Current.PlasmaGrid.ToString();
 
+            m_settingsView.Initialize();
 
-            m_cycleSpeed.Label.TextColor = Color.White;
-            m_gridSize.Label.TextColor = Color.White;
-            m_maxTailLength.Label.TextColor = Color.White;
-            m_suicide.Label.TextColor = Color.White;
-            m_drawGrid.Label.TextColor = Color.White;
-            m_playerName.Label.TextColor = Color.White;
-            m_plasmaGrid.Label.TextColor = Color.White;
-
-            var vp = GraphicsDevice.Viewport.Bounds;
-
-            m_OptionsPanel.Rect = RectangleBuilder.Centered(vp, new Vector2(0.6f, 0.45f));
-
-            m_cancelOk = new CancelOk(Game, this)
-                            {
-                                OnOk = ApplySettings,
-                                OnCancel = () => Game.ChangeState(GameState.MainMenu),
-                                OkText = "Apply",
-                                Rect = RectangleBuilder.BottomRight(vp, new Vector2(0.4f, 0.1f), new Point(5,5))
-                            };
+            m_settingsView.SettingsChanged += OnSettingsChanged;
         }
 
-        protected override void OnStateChanged(object sender, StateChangedEventArgs e)
+        public override void Update(GameTime gameTime)
         {
-            base.OnStateChanged(sender, e);
-            if (IsValidState)
-            {
-                m_gridSize.BoxText = Settings.Current.GridSize.Value.ToString();
-                m_cycleSpeed.BoxText = Settings.Current.CycleSpeed.Value.ToString();
-                m_maxTailLength.BoxText = Settings.Current.MaxTailLength.Value.ToString();
-                m_suicide.IsChecked = Settings.Current.AllowSuicide.Value;
-                m_drawGrid.IsChecked = Settings.Current.DrawGrid.Value;
-                m_playerName.BoxText = Settings.Current.PlayerName.Value;
-                m_plasmaGrid.IsChecked = Settings.Current.PlasmaGrid.Value;
-            }
-        }
-
-        private void ApplySettings()
-        {
-
-
-            try
-            {
-                Settings.Current.GridSize.TrySetValue(m_gridSize.BoxText);
-                Settings.Current.CycleSpeed.TrySetValue(m_cycleSpeed.BoxText);
-                Settings.Current.MaxTailLength.TrySetValue(m_maxTailLength.BoxText);
-                Settings.Current.AllowSuicide.TrySetValue(m_suicide.IsChecked);
-                Settings.Current.DrawGrid.TrySetValue(m_drawGrid.IsChecked);
-                Settings.Current.PlayerName.TrySetValue(m_playerName.BoxText);
-                Settings.Current.PlasmaGrid.TrySetValue(m_plasmaGrid.IsChecked);
-                
-                Settings.Current.WriteToFile();
-
-                Game.ChangeState(GameState.MainMenu);
-            }
-            catch (InvalidValueException)
-            {
-            }
-
+            base.Update(gameTime);
+            m_settingsView.Rect = RectangleBuilder.Centered(GraphicsDevice.Viewport.Bounds, new Vector2(0.7f, 0.6f));
         }
 
 
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
-            if (m_OptionsPanel.Visible)
+            if (m_settingsView.Visible)
             {
-                m_OptionsPanel.Draw(gameTime);
-            }
-
-            if (m_cancelOk.Visible)
-            {
-                m_cancelOk.Draw(gameTime);
+                m_settingsView.Draw(gameTime);
             }
         }
+
+        #endregion
+
+        #region Private and Protected Methods
+
+        protected override void HandleInupt(GameTime gameTime)
+        {
+            base.HandleInupt(gameTime);
+            if (Game.InputState.IsNewKeyPress(Keys.Escape))
+            {
+                Game.ChangeState(GameState.MainMenu);
+            }
+        }
+
+        private void OnSettingsChanged(object sender, SettingsChangedEventArgs e)
+        {
+            if (e.IsValid)
+            {
+                Settings.Current.WriteToFile();
+                Game.ChangeState(GameState.MainMenu);
+            }
+        }
+
+
+
+        protected override void OnStateChanged(object sender, StateChangedEventArgs e)
+        {
+            base.OnStateChanged(sender, e);
+            if (IsValidState)
+            {
+                m_settingsView.LoadSettings(Settings.Current);
+            }
+        }
+
+        #endregion
+
+
+
     }
 }
