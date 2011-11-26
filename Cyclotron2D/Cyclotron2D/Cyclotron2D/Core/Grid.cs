34d978290dad73df0106374521c98aaad239a820
darwish.michael@gmail.com <darwish.michael@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-26 22:36:29 +0000

    Added hoffman's pixel shader effect. note that if it doesn't compile for you, make sure you're using the HiDef profile instead of Reach (Project > Cyclotron2D Properties > XNA Game Studio > Game Profile)

--- Cyclotron2D/Cyclotron2D/Cyclotron2D/Core/Grid.cs
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/Core/Grid.cs
@@ -7,6 +7,8 @@ using Cyclotron2D.Helpers;
 using Cyclotron2D.Screens.Base;
 using Cyclotron2D.Screens.Main;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Cyclotron2D.Core.Players;
 
 namespace Cyclotron2D.Core
 {
@@ -17,6 +19,21 @@ namespace Cyclotron2D.Core
 		private float m_hue = 0;
 
         private float m_changeRate;
+
+		private SpriteBatch m_spriteBatch;
+
+		private RenderTarget2D m_renderTarget;
+		
+		private Effect m_effect;
+		
+		private VertexBuffer m_vertexBuffer;
+
+		private VertexPositionTexture[] m_vertices = {	new VertexPositionTexture(new Vector3(-1, -1, 0), Vector2.UnitY),
+														new VertexPositionTexture(new Vector3(-1, 1, 0), Vector2.Zero),
+														new VertexPositionTexture(new Vector3(1, -1, 0), Vector2.One),
+														new VertexPositionTexture(new Vector3(1, 1, 0), Vector2.UnitX) };
+
+		Dictionary<Cycle, Queue<Vector2>> cycleVelocities = new Dictionary<Cycle, Queue<Vector2>>();
 
         #endregion
 
@@ -53,7 +70,10 @@ namespace Cyclotron2D.Core
         public void Initialize(IEnumerable<Cycle> cycles)
         {
             Cycles = cycles.ToList();
-
+			foreach (var cycle in Cycles)
+			{
+				cycleVelocities[cycle] = new Queue<Vector2>();
+			}
             GridColor = Color.SteelBlue;
         }
 
@@ -75,7 +95,7 @@ namespace Cyclotron2D.Core
         /// <returns></returns>
         public Vector2 ToGridCoords(Point worldCoords)
         {
-            return new Vector2(worldCoords.X/(float)PixelsPerInterval, worldCoords.Y/(float)PixelsPerInterval);
+			return new Vector2(worldCoords.X / (float)PixelsPerInterval, worldCoords.Y / (float)PixelsPerInterval);
         }
 
         public override void Update(GameTime gameTime)
@@ -85,18 +105,65 @@ namespace Cyclotron2D.Core
 
             if((Screen as GameScreen).GameSettings.PlasmaGrid.Value)
                 UpdateColor(gameTime);
-            
+
+			UpdateCycleData(gameTime);
         }
 
-        public override void Draw(GameTime gameTime)
-        {
-            base.Draw(gameTime);
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
 
-            for (int i = 0; i < Size.X; i += PixelsPerInterval)
-                Game.SpriteBatch.Draw(Art.Pixel, new Rectangle(i, 0, 1, (int) Size.Y), GridColor);
-            for (int i = 0; i < Size.X; i += PixelsPerInterval)
-                Game.SpriteBatch.Draw(Art.Pixel, new Rectangle(0, i, (int) Size.X, 1), GridColor);
-        }
+			Game.GraphicsDevice.SetRenderTarget(m_renderTarget);
+			Game.GraphicsDevice.Clear(Color.Black);
+
+			m_spriteBatch.Begin();
+
+			for (int i = 0; i < Size.X; i += PixelsPerInterval)
+				m_spriteBatch.Draw(Art.Pixel, new Rectangle(i, 0, 1, (int)Size.Y), GridColor);
+			for (int i = 0; i < Size.X; i += PixelsPerInterval)
+				m_spriteBatch.Draw(Art.Pixel, new Rectangle(0, i, (int)Size.X, 1), GridColor);
+
+			m_spriteBatch.End();
+
+			Game.GraphicsDevice.SetRenderTarget(null);
+
+			var positions = new Vector2[Cycles.Count];
+			var velocities = new Vector2[Cycles.Count];
+			for (int i = 0; i < Cycles.Count && i < 6; i++)
+			{
+				var cycle = Cycles[i];
+
+				// Get average velocity
+				velocities[i] = cycleVelocities[cycle].Aggregate(Vector2.Zero, (x, y) => x + y, x => x / (cycleVelocities[cycle].Count * m_renderTarget.Bounds.Size()));
+				positions[i] = cycle.Position.ToVector() / m_renderTarget.Bounds.Size();
+			}
+
+			GraphicsDevice.SetVertexBuffer(m_vertexBuffer);
+			GraphicsDevice.SamplerStates[0] = SamplerState.LinearClamp;
+
+			m_effect.Parameters["inputTex"].SetValue(m_renderTarget);
+			m_effect.Parameters["numPlayers"].SetValue(Cycles.Count);
+			m_effect.Parameters["cyclePos"].SetValue(positions);
+			m_effect.Parameters["cycleVel"].SetValue(velocities);
+
+			foreach (EffectPass pass in m_effect.CurrentTechnique.Passes)
+			{
+				pass.Apply();
+				GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 2);
+			}
+		}
+
+		protected override void LoadContent()
+		{
+			m_spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+			m_renderTarget = new RenderTarget2D(Game.GraphicsDevice, Game.GraphicsDevice.PresentationParameters.BackBufferWidth, Game.GraphicsDevice.PresentationParameters.BackBufferHeight);
+
+			m_effect = Game.Content.Load<Effect>("Effect1");
+			m_effect.CurrentTechnique = m_effect.Techniques["Technique1"];
+
+			m_vertexBuffer = new VertexBuffer(Game.GraphicsDevice, VertexPositionTexture.VertexDeclaration, m_vertices.Length, BufferUsage.None);
+			m_vertexBuffer.SetData<VertexPositionTexture>(m_vertices);
+		}
 
         #endregion
 
@@ -118,6 +185,22 @@ namespace Cyclotron2D.Core
 			m_hue %= 6;
 			GridColor = new Vector3(m_hue, 1f, 0.5f).HSVToColor();
         }
+
+		private void UpdateCycleData(GameTime gameTime)
+		{
+			foreach (var cycle in Cycles)
+			{
+				// Because we define velocity solely in terms of direction (and not whether the cycle is
+				// actually moving), we need to check whether the game has started in order to determine 
+				// if the cycle is moving.
+				var cycleVelocity = (gameTime.TotalGameTime < cycle.GameStart) ? Vector2.Zero : cycle.Velocity;
+
+				var velocities = cycleVelocities[cycle];
+				velocities.Enqueue(cycleVelocity);
+				if (velocities.Count > 15)
+					velocities.Dequeue();
+			}
+		}
 
         #endregion
     }
