0581ef67524bc473dc7cce77772df385a1818794
stephane.beniak@gmail.com <stephane.beniak@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-28 00:50:39 +0000

    Added lame menu music :D

---
+++ branches/udp/Cyclotron2D/Cyclotron2D/Sounds/Sound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+
+namespace Cyclotron2D.Sounds
+{
+	static class Sound
+	{
+		public static Song MenuMusic;
+
+		public static bool menuMusicPlaying = false;
+
+		public static void Initialize()
+		{
+			MediaPlayer.IsRepeating = true;
+		}
+
+		public static void LoadContent(ContentManager content)
+		{
+			MenuMusic = content.Load<Song>(@"MenuMusic");
+		}
+
+	}
+}
