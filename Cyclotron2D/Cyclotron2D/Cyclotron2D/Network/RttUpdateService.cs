eb1fc5ff781bc29623569ef088ae2d258fdffa64
stephane.beniak@gmail.com <stephane.beniak@gmail.com@84d0f355-45fd-bc5c-03e2-06807eaf3ece>
2011-11-29 02:58:01 +0000

    nested locks yo

--- Cyclotron2D/Cyclotron2D/Cyclotron2D/Network/RttUpdateService.cs
+++ Cyclotron2D/Cyclotron2D/Cyclotron2D/Network/RttUpdateService.cs
@@ -12,118 +12,128 @@ using Microsoft.Xna.Framework;
 
 namespace Cyclotron2D.Network
 {
-    public class RttUpdateService : CyclotronComponent
-    {
+	public class RttUpdateService : CyclotronComponent
+	{
 
-        #region Fields
+		#region Fields
 
-        public static readonly TimeSpan DefaultUpdatePeriod = new TimeSpan(0, 0, 0, 1);
+		public static readonly TimeSpan DefaultUpdatePeriod = new TimeSpan(0, 0, 0, 1);
 
-        private TimeSpan m_lastPingRound;
+		private TimeSpan m_lastPingRound;
 
-        private Dictionary<NetworkConnection, TimeSpan> m_pingOutTimes;
+		private Dictionary<NetworkConnection, TimeSpan> m_pingOutTimes;
 
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Properties
 
-        public TimeSpan UpdatePeriod { get; set; }
-
-
-        #endregion
-
-        #region Constructor
-
-        public RttUpdateService(Game game)
-            : base(game)
-        {
-            m_pingOutTimes = new Dictionary<NetworkConnection, TimeSpan>();
-        }
-
-        #endregion
-
-        #region Public Methods
-
-        public override void Initialize()
-        {
-            base.Initialize();
-            SubscribeCommunicator();
-            UpdatePeriod = DefaultUpdatePeriod;
-        }
+		public TimeSpan UpdatePeriod { get; set; }
 
 
-        public void Reset()
-        {
-            lock (m_pingOutTimes)
-            {
-                m_pingOutTimes = new Dictionary<NetworkConnection, TimeSpan>();
-                foreach (NetworkConnection networkConnection in Game.Communicator.Connections.Values)
-                {
-                    m_pingOutTimes.Add(networkConnection, new TimeSpan(0));
-                }
-            }
-        }
+		#endregion
 
-        /// <summary>
-        /// this will resume the service if paused
-        /// </summary>
-        public void TriggerPing()
-        {
-            m_lastPingRound = new TimeSpan(0);
-        }
+		#region Constructor
 
-        public void Pause()
-        {
-            m_lastPingRound = TimeSpan.MaxValue - UpdatePeriod;
-        }
+		public RttUpdateService(Game game)
+			: base(game)
+		{
+			m_pingOutTimes = new Dictionary<NetworkConnection, TimeSpan>();
+		}
 
-        public void Resume()
-        {
-            TriggerPing();
-        }
+		#endregion
 
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            if (Game.IsState(GameState.PlayingAsHost | GameState.PlayingAsClient | GameState.GameLobbyHost | GameState.GameLobbyClient))
-            {
-                if (gameTime.TotalGameTime > m_lastPingRound + UpdatePeriod && m_pingOutTimes.Count > 0)
-                {
-                    lock (m_pingOutTimes)
-                    {
-                        foreach (var key in m_pingOutTimes.Keys.ToList())
-                        {
-							if (key != null) {
+		#region Public Methods
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			SubscribeCommunicator();
+			UpdatePeriod = DefaultUpdatePeriod;
+		}
+
+
+		public void Reset()
+		{
+			List<NetworkConnection> connections;
+			lock (Game.Communicator.Connections)
+			{
+				connections = Game.Communicator.Connections.Values.ToList();
+			}
+
+			lock (m_pingOutTimes)
+			{
+				m_pingOutTimes = new Dictionary<NetworkConnection, TimeSpan>();
+
+				foreach (NetworkConnection networkConnection in connections)
+				{
+					m_pingOutTimes.Add(networkConnection, new TimeSpan(0));
+				}
+
+
+			}
+		}
+
+		/// <summary>
+		/// this will resume the service if paused
+		/// </summary>
+		public void TriggerPing()
+		{
+			m_lastPingRound = new TimeSpan(0);
+		}
+
+		public void Pause()
+		{
+			m_lastPingRound = TimeSpan.MaxValue - UpdatePeriod;
+		}
+
+		public void Resume()
+		{
+			TriggerPing();
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			if (Game.IsState(GameState.PlayingAsHost | GameState.PlayingAsClient | GameState.GameLobbyHost | GameState.GameLobbyClient))
+			{
+				if (gameTime.TotalGameTime > m_lastPingRound + UpdatePeriod && m_pingOutTimes.Count > 0)
+				{
+					lock (m_pingOutTimes)
+					{
+						foreach (var key in m_pingOutTimes.Keys.ToList())
+						{
+							if (key != null)
+							{
 								m_pingOutTimes[key] = gameTime.TotalGameTime;
 							}
 						}
-                    }
-                    try
-                    {
-                        Game.Communicator.MessageAll(new NetworkMessage(MessageType.Ping, "out"));
-                        m_lastPingRound = gameTime.TotalGameTime;
-                    }
-                    catch (SocketException )
-                    {
-                        //a player has probably disconnected.
-                        //do nothing, the Communicator will inform us of this shortly and the connection will be removed from the list
-                    }
+					}
+					try
+					{
+						Game.Communicator.MessageAll(new NetworkMessage(MessageType.Ping, "out"));
+						m_lastPingRound = gameTime.TotalGameTime;
+					}
+					catch (SocketException)
+					{
+						//a player has probably disconnected.
+						//do nothing, the Communicator will inform us of this shortly and the connection will be removed from the list
+					}
 
-                }
-            }
-        }
+				}
+			}
+		}
 
 
-        #endregion
+		#endregion
 
-        #region Event Handlers
+		#region Event Handlers
 
-        private void OnMessageReceived(object sender, MessageEventArgs e)
-        {
-            if (Game.IsState(GameState.PlayingAsHost | GameState.PlayingAsClient | GameState.GameLobbyHost | GameState.GameLobbyClient))
-            {
-                if (e.Message.Type == MessageType.Ping)
-                {
+		private void OnMessageReceived(object sender, MessageEventArgs e)
+		{
+			if (Game.IsState(GameState.PlayingAsHost | GameState.PlayingAsClient | GameState.GameLobbyHost | GameState.GameLobbyClient))
+			{
+				if (e.Message.Type == MessageType.Ping)
+				{
 					Debug.Assert(e.Message.ContentLines.Count == 2 || e.Message.ContentLines.Count == 3, "Message must have 2 or 3 lines, fool");
 
 					var messageContent = e.Message.ContentLines[0];
@@ -141,81 +151,81 @@ namespace Cyclotron2D.Network
 						timeTakenToRespondIn = 0; // has to be assigned
 					}
 
-                    if(messageContent == "out")
-                    {
+					if (messageContent == "out")
+					{
 						var message = new NetworkMessage(MessageType.Ping, "in\n" + timeTakenToRespondOut);
-                        Game.Communicator.MessagePlayer(Game.Communicator.GetPlayer(e.Message.Source), message);
-                    }
-                    else if (messageContent == "in")
-                    {
-                        var connection = sender as NetworkConnection;
-                        {
-                            if (connection != null && m_pingOutTimes.ContainsKey(connection))
-                            {
-                                //Debug.Assert(m_pingOutTimes.ContainsKey(connection), "update too soon");
+						Game.Communicator.MessagePlayer(Game.Communicator.GetPlayer(e.Message.Source), message);
+					}
+					else if (messageContent == "in")
+					{
+						var connection = sender as NetworkConnection;
+						{
+							if (connection != null && m_pingOutTimes.ContainsKey(connection))
+							{
+								//Debug.Assert(m_pingOutTimes.ContainsKey(connection), "update too soon");
 								var timeTaken = new TimeSpan(0, 0, 0, 0, timeTakenToRespondOut + timeTakenToRespondIn);
 								var newRtt = Game.GameTime.TotalGameTime - m_pingOutTimes[connection] - timeTaken;
-                                connection.RoundTripTime = (connection.RoundTripTime + newRtt).Div(2);
+								connection.RoundTripTime = (connection.RoundTripTime + newRtt).Div(2);
 
-                                DebugMessages.AddLogOnly("Updated Rtt for " + Game.Communicator.GetPlayer(e.Message.Source) + " Rtt: " + connection.RoundTripTime);
-                                
-                            }
-                        }
-                    }
-                }
+								DebugMessages.AddLogOnly("Updated Rtt for " + Game.Communicator.GetPlayer(e.Message.Source) + " Rtt: " + connection.RoundTripTime);
 
-            }
-        }
+							}
+						}
+					}
+				}
 
-
-        private void OnConnectionLost(object sender, ConnectionEventArgs e)
-        {
-            lock (m_pingOutTimes)
-            {
-                if(m_pingOutTimes.ContainsKey(e.Connection))
-                {
-                    m_pingOutTimes.Remove(e.Connection);
-                }
-            }
-        }
+			}
+		}
 
 
-        #endregion
-
-        #region Private Methods
-        #endregion
-
-        #region Subscription
-
-        private void SubscribeCommunicator()
-        {
-            Game.Communicator.MessageReceived += OnMessageReceived;
-            Game.Communicator.ConnectionLost += OnConnectionLost;
-        }
+		private void OnConnectionLost(object sender, ConnectionEventArgs e)
+		{
+			lock (m_pingOutTimes)
+			{
+				if (m_pingOutTimes.ContainsKey(e.Connection))
+				{
+					m_pingOutTimes.Remove(e.Connection);
+				}
+			}
+		}
 
 
-        private void UnsubscribeCommunicator()
-        {
-            Game.Communicator.MessageReceived -= OnMessageReceived;
-            Game.Communicator.ConnectionLost -= OnConnectionLost;
-        }
+		#endregion
 
-        #endregion
+		#region Private Methods
+		#endregion
 
-        #region IDisposable
+		#region Subscription
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                UnsubscribeCommunicator();
-//                UnsubscribeGame();
-            }
-            base.Dispose(disposing);
-        }
+		private void SubscribeCommunicator()
+		{
+			Game.Communicator.MessageReceived += OnMessageReceived;
+			Game.Communicator.ConnectionLost += OnConnectionLost;
+		}
 
-        #endregion
 
-      
-    }
+		private void UnsubscribeCommunicator()
+		{
+			Game.Communicator.MessageReceived -= OnMessageReceived;
+			Game.Communicator.ConnectionLost -= OnConnectionLost;
+		}
+
+		#endregion
+
+		#region IDisposable
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				UnsubscribeCommunicator();
+				//                UnsubscribeGame();
+			}
+			base.Dispose(disposing);
+		}
+
+		#endregion
+
+
+	}
 }
